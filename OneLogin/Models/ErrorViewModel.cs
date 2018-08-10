82eca36d28dde73018dd6a90acfa02c3468c6431
Anvil A Chand <anvil.chand001@live.com>
2018-08-10 18:16:45 +1200

    added idp

---
+++ OneLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OneLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
